2f2528c426d71da684c2e61c561a5a2be1e973ff
Ichoku Chinonso <greatchinonso7@gmail.com>
2020-01-14 00:31:16 +0100

    using css modules on my gatsby blog

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Container from "../components/container"
+import styles from "./about-css-modules.module.css"
+
+
+
+export default () => (
+  <Container>
+    <h1>About CSS modules</h1>
+    <p>CSS modules are soo cool</p>
+  </Container>
+)
