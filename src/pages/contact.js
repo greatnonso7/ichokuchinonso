72e00c07fcdb0e7586293edc64f9c7c522cea328
Ichoku Chinonso <greatchinonso7@gmail.com>
2020-01-14 00:13:52 +0100

    Added linking to the gatsby blog

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Link to="/home">Home</Link>
+    <Link to="/">Home</Link>
     <Header headerText="Contact" />
     <p>Send us a message</p>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,12 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
+    <p>
+      <Link to="/contact/">Contact</Link>
+    </p>
+    <p>
+      <Link to="/about">About</Link>
+    </p>
     <Header headerText="Hello Gatsby" />
     <p>what a world</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
