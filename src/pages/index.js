4ce6d3599587d8db3dfd2a7d7b55153c10d7b989
Ichoku Chinonso <greatchinonso7@gmail.com>
2020-01-14 01:06:45 +0100

    added layouts to gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `purple` }}>
-    <p>
-      <Link to="/contact/">Contact</Link>
-    </p>
-    <p>
-      <Link to="/about">About</Link>
-    </p>
+  <Layout>
     <Header headerText="Hello Gatsby" />
     <p>what a world</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  </Layout>
 )
